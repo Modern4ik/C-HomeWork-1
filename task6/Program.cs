9b385344e69c6975ac6b9dbaff3c4764b21e3ef7
Sergey Zaytsev <surgery_333@mail.ru>
2023-04-14 18:56:36 +0300

    Выполнили третью задачу по поиску чётного числа и добавили файл task6

---
+++ task6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Определение числа на чётное или нечётное
+
+Console.Write("Введите целое число: ");
+string Number = Console.ReadLine() ?? "";
+int numb = int.Parse(Number);
+
+if (numb%2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечётное");
+}
