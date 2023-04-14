4fbb1bc023846e6a96af1bf7e62d673077a6a5b4
Sergey Zaytsev <surgery_333@mail.ru>
2023-04-14 19:25:50 +0300

    выполнили задачу по поиску чётного в заданном пределе и добавили файл task8

---
+++ task8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Поиск чётного числа в заданном пределе
+
+Console.Write("Введите число: ");
+string Number = Console.ReadLine() ?? "";
+int limit = int.Parse(Number);
+
+int numb = 1;
+
+while (numb <= limit)
+{
+    if (numb%2 == 0)
+        Console.WriteLine(numb);
+    numb = numb + 1;
+}
