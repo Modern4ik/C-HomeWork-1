3608068a1a62a6f9d33458499bda5e266d4e923a
Sergey Zaytsev <surgery_333@mail.ru>
2023-04-14 18:25:22 +0300

    Выполнили первую задачу в task2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Поиск максимального числа из двух целых чисел
+
+Console.Write("Введите первое целое число: ");
+String FirstNumber = Console.ReadLine() ?? "";
+int number1 = int.Parse(FirstNumber);
+Console.Write("Введите второе целое число: ");
+String SecondNumber = Console.ReadLine() ?? "";
+int number2 = int.Parse(SecondNumber);
+
+if (number1 > number2)
+{
+    Console.WriteLine(number1);
+}
+else
+{
+    Console.WriteLine(number2);
+}
